abb223c0404b28e0dee0e8b3901077174d8e9d44
Mehedi Hasan メヘディ ハサン <mehedihasan.cse.jp@gmail.com>
2020-06-07 13:44:22 +0600

    logo and card

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,9 +1,84 @@
 import 'package:flutter/material.dart';
-class LoginScreen extends StatelessWidget {
+
+class LoginScreen extends StatefulWidget {
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return SafeArea(
+      child: Scaffold(
+        body: Padding(
+          padding: const EdgeInsets.all(15.0),
+          child: Container(
+            child: Column(
+              children: <Widget>[
+                Container(
+                  child: Row(
+                    children: <Widget>[
+                      Text(
+                        "e",
+                        style: TextStyle(
+                          color: Colors.blue,
+                          fontSize: 40.0,
+                        ),
+                      ),
+                      Text(
+                        "Daktar",
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 40.0,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 40.0,
+                ),
+                Container(
+                  child: Stack(
+                    children: <Widget>[
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(20),
+                        child: Image(
+                          image: AssetImage('images/index.jpeg'),
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.symmetric(
+                            vertical: 50.0, horizontal: 20.0),
+                        child: Text(
+                          "Online Health Care",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 24.0,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.symmetric(
+                            vertical: 80.0, horizontal: 20.0),
+                        child: Text(
+                          "24 Hours Service Availavle",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 18.0,
+                            fontWeight: FontWeight.normal,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
